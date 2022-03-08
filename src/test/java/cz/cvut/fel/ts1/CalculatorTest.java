97b50facd74972bd4bc369cc9b136c404f55fe10
tuparobe <tuparobe@fel.cvut.cz>
2022-03-09 00:03:56 +0100

    hw2_divisionByZero

--- src/test/java/cz/cvut/fel/ts1/CalculatorTest.java
+++ src/test/java/cz/cvut/fel/ts1/CalculatorTest.java
@@ -1,5 +1,6 @@
 package cz.cvut.fel.ts1;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -13,17 +14,23 @@ public class CalculatorTest {
     }
 
     @Test
-    public void substract_ThreeMinusThree_isZero() {
+    public void substract_threeMinusThree_isZero() {
         assertEquals(0, calc.subtract(3, 3));
     }
 
     @Test
-    public void multiply_TwoMultiplyByThree_isSix() {
+    public void multiply_twoMultiplyByThree_isSix() {
         assertEquals(6, calc.multiply(2, 3));
     }
 
     @Test
-    public void divide_SixDividedByTwo_isThree() {
+    public void divide_sixDividedByTwo_isThree() {
         assertEquals(3, calc.divide(6, 2));
+    }
+
+    @Test
+    public void divide_byZero_isException() {
+        Exception exception = Assertions.assertThrows(Exception.class, () -> calc.divide(20, 0));
+        assertEquals("/ by zero", exception.getMessage());
     }
 }
